570d92d6e90a8dbbf6e64bb8dd871d541c5a19b3
Juarez Coutinho <juarezcout15.jl@gmail.com>
2023-04-25 14:41:48 +0100

    first commit

---
+++ src/styles/index.js
@@ -0,0 +1,59 @@
+export const Theme = {
+  colors: {
+    primary: "#033D60",
+    primary_light: "#026A78",
+    primary_dark: "#022C78",
+
+    secondary: "#d4d9e2",
+    secondary_dark: "#A5A9B0",
+
+    success: "#12A455",
+    success_light: "#75efac",
+
+    error: "#E83F5b",
+    error_light: "#ee768a",
+
+    warning: "#F5A000",
+    warning_light: "#F7BA4A",
+
+    border: "#B5B5B5",
+
+    light: "#fff",
+    dark: "#000",
+
+    text_light: "#e7e3dee4",
+    text_dark: "#2B2B2B",
+
+    background: "#F1F1F1",
+    background_light: "#F7F7F7",
+  },
+  sizes: {
+    fontSizeXXL: 24,
+    fontSizeXL: 18,
+    fontSizeL: 16,
+    fontSizeM: 14,
+    fontSizeS: 12,
+    fontSizeXS: 10,
+
+    paddingL: 20,
+    paddingM: 15,
+    paddingS: 10,
+    paddingXS: 5,
+
+    marginL: 20,
+    marginM: 15,
+    marginS: 10,
+    marginXS: 5,
+
+    borderRadiusXL: 150,
+    borderRadiusL: 50,
+    borderRadiusM: 25,
+    borderRadiusS: 5,
+
+    heightXL: 150,
+    heightL: 120,
+    heightM: 55,
+    heightS: 45,
+    heightXS: 30,
+  },
+};
